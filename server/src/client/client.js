d54ade546c4b49948874d3e49745f68a9355db04
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-05 13:40:18 -0700

    Route Configuration

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,6 +1,11 @@
 // Startup for the client side application.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
+    , document.querySelector('#root'));
