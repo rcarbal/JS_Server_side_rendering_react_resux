8c669183347e8cfeb8c7f5b5f78237d3113e27e6
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-05 12:36:30 -0700

    Mergin Webpack Config

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,28 +1,15 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const base = require('./webpack.base.js')
 
-module.exports = {
+const config =  {
      // Tell webpack the root file of our server application.
      entry: './src/client/client.js', 
     // Tells webpack where to put the output file that is generated
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
-    },
-    // Tell webpack to run babel on every file it runs through
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options:{
-                    presets:[
-                        'react',
-                        'stage-0',
-                        ['env', { targets: { browsers: ['last 2 versions'] }}]
-                    ]
-                }
-            }
-        ]
     }
 }
+
+module.exports = merge(base, config);
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
-
-module.exports = {
+const merge = require('webpack-merge');
+const base = require('./webpack.base.js')
+const config = {
     /**
      * Inform webpack that we are building a bundle for NodeJS
      * Rather than for the browser
@@ -14,22 +15,7 @@ module.exports = {
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    },
-    // Tell webpack to run babel on every file it runs through
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options:{
-                    presets:[
-                        'react',
-                        'stage-0',
-                        ['env', { targets: { browsers: ['last 2 versions'] }}]
-                    ]
-                }
-            }
-        ]
     }
 }
+
+module.exports = merge(base, config);
