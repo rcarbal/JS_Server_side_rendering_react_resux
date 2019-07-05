30a5051b73fe27685215f2736763e3f975e1ed65
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-05 09:31:42 -0700

    Client Bundles

---
+++ server/src/client/client.js
@@ -0,0 +1,3 @@
+// Startup for the client side application.
+
+console.log("Hi there")
---
+++ server/webpack.client.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+     // Tell webpack the root file of our server application.
+     entry: './src/client/client.js', 
+    // Tells webpack where to put the output file that is generated
+    output:{
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    // Tell webpack to run babel on every file it runs through
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options:{
+                    presets:[
+                        'react',
+                        'stage-0',
+                        ['env', { targets: { browsers: ['last 2 versions'] }}]
+                    ]
+                }
+            }
+        ]
+    }
+}
