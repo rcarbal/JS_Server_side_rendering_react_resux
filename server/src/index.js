cff2998d4d44739cf0b862118ef95012fddc4be4
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-10 08:49:52 -0700

    Server Store Creation

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static("public"));
 app.get('*', (req, res) => {
+    const store = createStore();
     
-    res.send(renderer(req));    
+    res.send(renderer(req, store));    
 });
 
 const PORT = 3000;
