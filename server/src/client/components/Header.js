589f6a68cd0fe35065db001093be64d93b8ec3b1
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-18 09:45:28 -0700

    Connecting the Header

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import  { connect } from 'react-redux';
 
-export default () => {
+const Header = ({ auth }) => {
+    console.log('My auth status is ', auth);
     return (<div>
         <Link to="/">React SSR</Link>
     </div>
     );
+};
+
+// auth is being destrucured off the state
+function mapStateToPros({ auth }){
+    return { auth };
 }
+
+export default connect(mapStateToPros)(Header);
